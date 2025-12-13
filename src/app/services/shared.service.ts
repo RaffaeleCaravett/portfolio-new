3770b4922d40b531850e24ba7f5289794fc837fc
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-12-13 20:49:21 +0100

    doing tasks

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -72,14 +72,14 @@ export class SharedService {
       route: 'snake',
     },
     {
-      id: 8,
+      id: 9,
       title: 'Three.js',
       description: 'Explore some of the numerous Three.js features and tricks.',
       image: 'assets/idea/three.jpg',
       route: 'three',
     },
     {
-      id: 9,
+      id: 10,
       title: 'Tools',
       description:
         'A section where i use some of the tools from primeng, angular material and other external libraries.',
@@ -87,7 +87,7 @@ export class SharedService {
       route: 'tools',
     },
     {
-      id: 10,
+      id: 11,
       title: 'Informations',
       description:
         'A readme.md of this application. I provide you general informations about technologies used, features and use cases of this website.',
@@ -95,7 +95,7 @@ export class SharedService {
       route: 'info',
     },
     {
-      id: 11,
+      id: 12,
       title: 'About me',
       description:
         'A more personal section, where you can read about me, my studies, my passions and how i spend my life.',
@@ -103,13 +103,13 @@ export class SharedService {
       route: 'about',
     },
     {
-      id: 12,
+      id: 13,
       title: 'Error route',
       description:
-        'A specific wrong route to show the error component, activated in case the desired route doesn\'t match with declared routes.',
+        "A specific wrong route to show the error component, activated in case the desired route doesn't match with declared routes.",
       image: 'assets/idea/wrong.jpg',
       route: 'wrong',
-    }
+    },
   ];
 
   getSlides(): {
