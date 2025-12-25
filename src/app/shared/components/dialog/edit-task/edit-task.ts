a9f587c2e8a47b3610cb96e955e8526ab7a83190
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-12-25 21:45:07 +0100

    nice version of task app

--- src/app/shared/components/dialog/edit-task/edit-task.ts
+++ src/app/shared/components/dialog/edit-task/edit-task.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Inject, OnInit } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogContent,
@@ -6,10 +6,12 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { TaskStates } from '../../../enums/enums';
+import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-task',
-  imports: [MatDialogTitle, MatDialogContent],
+  imports: [MatDialogTitle, MatDialogContent, NgxSkeletonLoaderComponent, ReactiveFormsModule],
   templateUrl: './edit-task.html',
   styleUrl: './edit-task.scss',
 })
@@ -23,6 +25,18 @@ export class EditTaskComponent implements OnInit {
     createdAt: string;
     description: string;
   };
+  protected taskForm: FormGroup = new FormGroup({});
+  protected users: { id: number; fullname: string }[] = [
+    { id: 1, fullname: 'Mario Rossi' },
+    { id: 2, fullname: 'Luigi Bianchi' },
+    { id: 3, fullname: 'Sesti Osseo' },
+  ];
+  protected states: { state: TaskStates }[] = [
+    { state: TaskStates.TO_DO },
+    { state: TaskStates.PROCESS },
+    { state: TaskStates.COMPLETE },
+  ];
+  protected cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
   constructor(
     public dialogRef: MatDialogRef<EditTaskComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -31,5 +45,16 @@ export class EditTaskComponent implements OnInit {
   ngOnInit(): void {
     this.showSkeleton = true;
     this.task = this.data;
+    this.taskForm = new FormGroup({
+      title: new FormControl(this.task.name, Validators.required),
+      state: new FormControl(this.task.state, Validators.required),
+      assignedTo: new FormControl(this.task.assignedTo, Validators.required),
+      description: new FormControl(this.task.description, Validators.required),
+      createdAt: new FormControl(this.task.createdAt, Validators.required),
+    });
+    setTimeout(() => {
+      this.showSkeleton = false;
+      this.cdr.markForCheck();
+    }, 2500);
   }
 }
